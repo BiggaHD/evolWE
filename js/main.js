120f2a0096c71675cc12b0cfdec85cd6e0fcb12f
Dzenis <dzenis.hankusic@gmail.com>
2022-10-17 21:57:36 +0200

    hotFix 2

--- js/main.js
+++ js/main.js
@@ -22,3 +22,17 @@ $("#navbar a, .btn").on("click", function(event) {
     );
   }
 });
+
+
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 25.0657, lng: 55.17128 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
